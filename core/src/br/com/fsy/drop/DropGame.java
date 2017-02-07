aea798770ec52479bce85274a7aef3e3385a80b7
F0113091 <fabianoyoschitaki@gmail.com>
2017-02-07 20:15:27 -0200

    apk

--- core/src/br/com/fsy/drop/DropGame.java
+++ core/src/br/com/fsy/drop/DropGame.java
@@ -4,7 +4,6 @@ import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.audio.Music;
 import com.badlogic.gdx.audio.Sound;
-import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
@@ -32,6 +31,9 @@ public class DropGame extends ApplicationAdapter {
 	private Array<Integer> raindropValues;
 	private long lastDropTime;
 
+	private int lives;
+	private Texture heartImage;
+
 	public static int score;
 	public static int raindropWithResult;
 	public static int scoreYPosition;
@@ -43,6 +45,7 @@ public class DropGame extends ApplicationAdapter {
 	public static float gameHeight;
 
 	private static int bucketWidthHeight;
+	private static int heartWidthHeight;
 	private static int dropWidthHeight;
 
 	private static int bucketYPosition;
@@ -56,41 +59,37 @@ public class DropGame extends ApplicationAdapter {
 	private static int operacao;
 
 	public static final int MAX_DROPS = 5;
+	private boolean isGameRunning;
 
 	@Override
 	public void create() {
-		score = 0;
-		raindropWithResult = 0;
-		isRaindropWithResultAlive = false;
-
 		_screenWidth = Gdx.graphics.getWidth();
 		_screenHeight = Gdx.graphics.getHeight();
 		gameWidth = 136;
 		gameHeight = _screenHeight / (_screenWidth / gameWidth);
 		scoreYPosition = (int) (gameHeight * .9);
 
-		bucketWidthHeight = (int) (gameHeight * .1);
-		dropWidthHeight = bucketWidthHeight;
+		bucketWidthHeight = (int) (gameHeight * .07);
+		dropWidthHeight = (int) (gameHeight * .07);
+		heartWidthHeight = (int) (gameHeight * .07);
 
 		bucketYPosition = (int) (gameHeight * .1);
 
 		font = new BitmapFont(Gdx.files.internal("text.fnt"));
-		font.getData().setScale(gameHeight * 0.0011f, gameHeight * 0.0011f);
+		font.getData().setScale(gameHeight * 0.0008f, gameHeight * 0.0008f);
 
 		shadow = new BitmapFont(Gdx.files.internal("shadow.fnt"));
-		shadow.getData().setScale(gameHeight * 0.0011f, gameHeight * 0.0011f);
+		shadow.getData().setScale(gameHeight * 0.0008f, gameHeight * 0.0008f);
 
 		// load the images for the droplet and the bucket, 64x64 pixels each
 		dropImage = new Texture(Gdx.files.internal("droplet.png"));
 		bucketImage = new Texture(Gdx.files.internal("bucket.png"));
+		heartImage = new Texture(Gdx.files.internal("heart.png"));
 
 		// load the drop sound effect and the rain background "music"
 		dropSound = Gdx.audio.newSound(Gdx.files.internal("drop.wav"));
 		rainMusic = Gdx.audio.newMusic(Gdx.files.internal("rain.mp3"));
-
-		// start the playback of the background music immediately
 		rainMusic.setLooping(true);
-		rainMusic.play();
 
 		// create the camera and the SpriteBatch
 		camera = new OrthographicCamera();
@@ -110,7 +109,16 @@ public class DropGame extends ApplicationAdapter {
 		bucket.x = (gameWidth/2) - (bucket.width /2); // center the bucket horizontally
 		bucket.y = bucketYPosition; // bottom left corner of the bucket is 20 pixels above the bottom screen edge
 
-		// create the raindrops array and spawn the first raindrop
+		startGame();
+	}
+
+	private void startGame() {
+		isGameRunning = true;
+		rainMusic.play();
+		lives = 3;
+		score = 0;
+		raindropWithResult = 0;
+		isRaindropWithResultAlive = false;
 		raindrops = new Array<Rectangle>();
 		raindropValues = new Array<Integer>();
 		aleatorizaNumeros();
@@ -171,84 +179,145 @@ public class DropGame extends ApplicationAdapter {
 
 	@Override
 	public void render() {
-		// clear the screen with a dark blue color. The
-		// arguments to glClearColor are the red, green
-		// blue and alpha component in the range [0,1]
-		// of the color to be used to clear the screen.
-		Gdx.gl.glClearColor(0, 0, 0.2f, 1);
-		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+		if (isGameRunning) {
+			// clear the screen with a dark blue color. The
+			// arguments to glClearColor are the red, green
+			// blue and alpha component in the range [0,1]
+			// of the color to be used to clear the screen.
+			Gdx.gl.glClearColor(0, 0, 0.2f, 1);
+			Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 
-		// tell the camera to update its matrices.
-		camera.update();
+			// tell the camera to update its matrices.
+			camera.update();
 
-		// tell the SpriteBatch to render in the
-		// coordinate system specified by the camera.
-		batch.setProjectionMatrix(camera.combined);
+			// tell the SpriteBatch to render in the
+			// coordinate system specified by the camera.
+			batch.setProjectionMatrix(camera.combined);
 
-		// begin a new batch and draw the bucket and
-		// all drops
-		batch.begin();
-		drawBucket();
-		drawDrops();
-		drawScore();
-		batch.end();
+			// begin a new batch and draw the bucket and
+			// all drops
+			batch.begin();
+			drawBucket();
+			drawDrops();
+			drawScore();
+			drawLives();
+			batch.end();
 
-		// process user input
-		if(Gdx.input.isTouched()) {
-			Vector3 touchPos = new Vector3();
-			touchPos.set(Gdx.input.getX(), Gdx.input.getY(), 0);
-		//Gdx.app.log("DropGame", "Before unproject: " + touchPos.x + ", " + touchPos.y + ", " + touchPos.z);
-			camera.unproject(touchPos);
-			bucket.x = touchPos.x - (bucketWidthHeight / 2);
-			//Gdx.app.log("DropGame", "After unproject: " + touchPos.x + ", " + touchPos.y + ", " + touchPos.z);
-		}
-		//if(Gdx.input.isKeyPressed(Input.Keys.LEFT)) {
-		//	bucket.x -= GAME_VELOCITY * Gdx.graphics.getDeltaTime();
-		//}
-		//if(Gdx.input.isKeyPressed(Input.Keys.RIGHT)) {
-		//	bucket.x += GAME_VELOCITY * Gdx.graphics.getDeltaTime();
-		//}
-
-		// make sure the bucket stays within the screen bounds
-		if(bucket.x < 0) {
-			bucket.x = 0;
-		}
-		if(bucket.x > gameWidth - bucketWidthHeight) {
-			bucket.x = gameHeight - bucketWidthHeight;
-		}
-
-		// check if we need to create a new raindrop
-		if (TimeUtils.nanoTime() - lastDropTime > DROP_RESPAWN_TIME_IN_MILLIS && raindrops.size < MAX_DROPS) {
-			spawnRaindrop();
-		}
-
-		// move the raindrops, remove any that are beneath the bottom edge of
-		// the screen or that hit the bucket. In the later case we play back
-		// a sound effect as well.
-		Iterator<Rectangle> iter = raindrops.iterator();
-		int cont = 0;
-		while(iter.hasNext()) {
-			Rectangle raindrop = iter.next();
-			raindrop.y -= GAME_VELOCITY * Gdx.graphics.getDeltaTime();
-			if (raindrop.y + dropWidthHeight < 0) {
-				resetRainDropPosition(raindrop);
+			// process user input
+			if (Gdx.input.isTouched()) {
+				Vector3 touchPos = new Vector3();
+				touchPos.set(Gdx.input.getX(), Gdx.input.getY(), 0);
+				//Gdx.app.log("DropGame", "Before unproject: " + touchPos.x + ", " + touchPos.y + ", " + touchPos.z);
+				camera.unproject(touchPos);
+				bucket.x = touchPos.x - (bucketWidthHeight / 2);
+				//Gdx.app.log("DropGame", "After unproject: " + touchPos.x + ", " + touchPos.y + ", " + touchPos.z);
 			}
-			if (raindrop.overlaps(bucket)) {
-				//
-				if (resultado == raindropValues.get(cont)) {
-					aleatorizaNumeros();
-					dropSound.play();
-					score += 1;
-					iter.remove();
-					raindropValues.removeIndex(cont);
-				} else {
-					score -= 1;
-					dropSound.play();
-					iter.remove();
-					raindropValues.removeIndex(cont);
+			//if(Gdx.input.isKeyPressed(Input.Keys.LEFT)) {
+			//	bucket.x -= GAME_VELOCITY * Gdx.graphics.getDeltaTime();
+			//}
+			//if(Gdx.input.isKeyPressed(Input.Keys.RIGHT)) {
+			//	bucket.x += GAME_VELOCITY * Gdx.graphics.getDeltaTime();
+			//}
+
+			// make sure the bucket stays within the screen bounds
+			if (bucket.x < 0) {
+				bucket.x = 0;
+			}
+			if (bucket.x > gameWidth - bucketWidthHeight) {
+				bucket.x = gameHeight - bucketWidthHeight;
+			}
+
+			// check if we need to create a new raindrop
+			if (TimeUtils.nanoTime() - lastDropTime > DROP_RESPAWN_TIME_IN_MILLIS && raindrops.size < MAX_DROPS) {
+				spawnRaindrop();
+			}
+
+			// move the raindrops, remove any that are beneath the bottom edge of
+			// the screen or that hit the bucket. In the later case we play back
+			// a sound effect as well.
+			Iterator<Rectangle> iter = raindrops.iterator();
+			int cont = 0;
+			while (iter.hasNext()) {
+				Rectangle raindrop = iter.next();
+				raindrop.y -= GAME_VELOCITY * Gdx.graphics.getDeltaTime();
+				if (raindrop.y + dropWidthHeight < 0) {
+					resetRainDropPosition(raindrop);
+				}
+				if (raindrop.overlaps(bucket)) {
+					if (resultado == raindropValues.get(cont)) {
+						aleatorizaNumeros();
+						dropSound.play();
+						score += 1;
+						iter.remove();
+						raindropValues.removeIndex(cont);
+					} else {
+						lives -= 1;
+						dropSound.play();
+						iter.remove();
+						raindropValues.removeIndex(cont);
+						if (lives == 0){
+							isGameRunning = false;
+						}
+					}
+				}
+				cont++;
+			}
+		} else {
+			// clear the screen with a dark blue color. The
+			// arguments to glClearColor are the red, green
+			// blue and alpha component in the range [0,1]
+			// of the color to be used to clear the screen.
+			Gdx.gl.glClearColor(0, 0, 0.2f, 1);
+			Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+
+			// tell the camera to update its matrices.
+			camera.update();
+
+			// tell the SpriteBatch to render in the
+			// coordinate system specified by the camera.
+			batch.setProjectionMatrix(camera.combined);
+
+			// begin a new batch and draw the bucket and
+			// all drops
+			batch.begin();
+			drawGameOver();
+			pauseSounds();
+			batch.end();
+
+			if (Gdx.input.isTouched()) {
+				Vector3 touchPos = new Vector3();
+				touchPos.set(Gdx.input.getX(), Gdx.input.getY(), 0);
+				camera.unproject(touchPos);
+				if (touchPos.y < (gameHeight/2) + 20 && touchPos.y > (gameHeight/2) - 20) {
+					startGame();
 				}
 			}
-			cont++;
+		}
+	}
+
+	private void pauseSounds() {
+		rainMusic.stop();
+	}
+
+	private void drawGameOver() {
+		int length = ("Touch to restart" + score).length();
+		shadow.draw(batch, "Touch to restart", gameWidth/2 - (3 * length), gameHeight/2);
+		font.draw(batch, "Touch to restart", gameWidth/2 - (3 * length), (gameHeight/2) - 1);
+
+		length = ("Game Over!" + score).length();
+		shadow.draw(batch, "Game Over!", gameWidth/2 - (3 * length), (int)(gameHeight * 1.2)/2);
+		font.draw(batch, "Game Over!", gameWidth/2 - (3 * length), (int)(gameHeight * 1.2/2) - 1);
+	}
+
+	private void drawLives() {
+		if  (lives >= 3){
+			batch.draw(heartImage, (int) (gameWidth * .65), (int)(gameHeight * .9), heartWidthHeight, heartWidthHeight);
+		}
+		if (lives >= 2){
+			batch.draw(heartImage, (int) (gameWidth * .65) + heartWidthHeight, (int)(gameHeight * .9), heartWidthHeight, heartWidthHeight);
+		}
+		if (lives >= 1){
+			batch.draw(heartImage, (int) (gameWidth * .65) + 2 * heartWidthHeight, (int)(gameHeight * .9), heartWidthHeight, heartWidthHeight);
 		}
 	}
 
@@ -256,16 +325,16 @@ public class DropGame extends ApplicationAdapter {
 		for (int cont = 0; cont < raindrops.size; cont++) {
 			batch.draw(dropImage, raindrops.get(cont).x, raindrops.get(cont).y, dropWidthHeight, dropWidthHeight);
 			int length = (raindropValues.get(cont) + "").length();
-			shadow.draw(batch, raindropValues.get(cont) + "", raindrops.get(cont).x, raindrops.get(cont).y + (int)(dropWidthHeight * 1.7));
-			font.draw(batch, raindropValues.get(cont) + "", raindrops.get(cont).x, (raindrops.get(cont).y + (int)(dropWidthHeight * 1.7)) - 1 );
+			shadow.draw(batch, raindropValues.get(cont) + "", raindrops.get(cont).x, raindrops.get(cont).y + (int)(dropWidthHeight * 2));
+			font.draw(batch, raindropValues.get(cont) + "", raindrops.get(cont).x, (raindrops.get(cont).y + (int)(dropWidthHeight * 2)) - 1 );
 		}
 	}
 
 	private void drawBucket() {
 		batch.draw(bucketImage, bucket.x, bucket.y, bucketWidthHeight, bucketWidthHeight);
 		int length = (bucketCalculation).length();
-		shadow.draw(batch, bucketCalculation, gameWidth/2 - (5 * length), bucket.y);
-		font.draw(batch, bucketCalculation, gameWidth/2 - (5 * length), bucket.y);
+		shadow.draw(batch, bucketCalculation, gameWidth/2 - (4 * length), bucket.y);
+		font.draw(batch, bucketCalculation, gameWidth/2 - (4 * length), bucket.y-1);
 	}
 
 	private void drawScore() {
